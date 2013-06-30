84c646baf043a9eb35ec313c86f0a7086b43a3e0
Yariv Katz <yariv@nerdeez.com>
2013-06-30 14:10:32 +0300

    trying to fix heroku unicorn problem

--- nerdeez_server_app/settings.py
+++ nerdeez_server_app/settings.py
@@ -166,8 +166,8 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 INSTALLED_APPS = INSTALLED_APPS + ('gunicorn',)
-INSTALLED_APPS = INSTALLED_APPS + ('nerdeez_server_app',)
 INSTALLED_APPS = INSTALLED_APPS + ('south',)
+INSTALLED_APPS = INSTALLED_APPS + ('nerdeez_server_app',)
 INSTALLED_APPS = INSTALLED_APPS + ('tastypie',)
 
 #s3 storage
